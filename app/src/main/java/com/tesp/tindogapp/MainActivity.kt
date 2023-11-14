79d035f6c061c22801eabe55556a9806df163dc1
joaocotrimcosta <115737003+joaocotrimcosta@users.noreply.github.com>
2023-11-14 23:24:56 +0000

    TINDOG-00 Criação da navegação inicial

--- app/src/main/java/com/tesp/tindogapp/MainActivity.kt
+++ app/src/main/java/com/tesp/tindogapp/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tesp.tindogapp.pages.*
+//import com.tesp.tindogapp.pages.paginaB
 import com.tesp.tindogapp.ui.theme.TinDogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +24,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "login") {
+                        composable("login") {
+                            paginaA(navController)
+                        }
+                        composable("criarConta") {
+                            paginaB(navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TinDogAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/tesp/tindogapp/pages/paginaA.kt
@@ -0,0 +1,13 @@
+package com.tesp.tindogapp.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+@Preview
+fun paginaA(navController: NavHostController= rememberNavController()) {
+    Text(text = "Página A")
+}
