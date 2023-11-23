300a6d9019c2018ea1987cc2c30096fe8063a4b8
Pedro Torrezão <ptorrezao@users.noreply.github.com>
2023-11-23 20:22:49 +0000

    Merge

--- app/src/main/java/com/tesp/tindogapp/MainActivity.kt
+++ app/src/main/java/com/tesp/tindogapp/MainActivity.kt
@@ -38,6 +38,9 @@ class MainActivity : ComponentActivity() {
                         composable("signPage") {
                             SignInPage(navController)
                         }
+                        composable("match") {
+                            likeDislike(navController)
+                        }
                     }
                 }
             }
