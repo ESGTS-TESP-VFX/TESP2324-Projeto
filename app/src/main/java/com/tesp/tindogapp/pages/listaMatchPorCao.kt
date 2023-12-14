0504dbf879cc91176e9596641bf6adf2866e072f
allen-andrade <allenandrade@gmail.com>
2023-12-14 21:19:52 +0000

    TINDOG-58 Lista de Match Por Cao Adaptação do botão do cão para o lado direito, para permitir o menu de opções.

--- app/src/main/java/com/tesp/tindogapp/pages/listaMatchPorCao.kt
+++ app/src/main/java/com/tesp/tindogapp/pages/listaMatchPorCao.kt
@@ -34,137 +34,92 @@ import androidx.navigation.compose.rememberNavController
 import com.tesp.tindogapp.R
 import com.tesp.tindogapp.components.footerBeMyFriend
 
-@Preview(showBackground = true, widthDp = 380)
+@Preview(showBackground = true, widthDp = 380, heightDp = 1300)
 @Composable
 fun MatchPorCao(navHostController: NavHostController = rememberNavController()): Unit {
 
-    val match1 = listOf(
+    val listaCao = listOf(
+        R.drawable.fotocao4,
         R.drawable.fotocao1,
-        R.drawable.fotocao2,
-        R.drawable.fotocao3,
+        R.drawable.fotocao2
     )
 
-    val match2 = listOf(
-        R.drawable.fotocao4,
-        R.drawable.fotocao5
+    val matches = listOf(
+        listOf(
+            R.drawable.fotocao1,
+            R.drawable.fotocao2,
+            R.drawable.fotocao3,
+        ),
+        listOf(
+            R.drawable.fotocao4,
+            R.drawable.fotocao5
+        ),
+        listOf(
+            R.drawable.fotocao3,
+            R.drawable.fotocao1,
+            R.drawable.fotocao4,
+            R.drawable.fotocao5
+        )
     )
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
-    )
-    {
-        Box {
-
-            // Galeria de matchs do cão1
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 70.dp)
-            )
-            {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    contentPadding = PaddingValues(8.dp)
+    ) {
+        for (i in matches.indices) {
+            Box {
+                // Galeria de matches
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 70.dp, bottom = 20.dp)
                 ) {
-                    items(match1.size) { index ->
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(8.dp)
+                    ) {
+                        items(matches[i].size) { index ->
+                            Image(
+                                painter = painterResource(id = matches[i][index]),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .aspectRatio(1f)
+                                    .padding(4.dp)
+                                    .clip(RoundedCornerShape(20.dp))
+                                    .background(MaterialTheme.colorScheme.background)
+                                    .clickable { /* Handle click if needed */ },
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
+                // Botão redondo
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                        contentPadding = PaddingValues(),
+                        modifier = Modifier
+                            .size(100.dp)
+                            .border(
+                                BorderStroke(4.dp, Color.Green),
+                                CircleShape
+                            )
+                            .clip(CircleShape)
+                    ) {
                         Image(
-                            painter = painterResource(id = match1[index]),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1f)
-                                .padding(4.dp)
-                                .clip(RoundedCornerShape(20.dp))
-                                .background(MaterialTheme.colorScheme.background)
-                                .clickable { /* Handle click if needed */ },
-                            contentScale = ContentScale.Crop
+                            painter = painterResource(id = listaCao[i]),
+                            contentDescription = "botao_redondo"
                         )
                     }
                 }
-            }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Start
-            ) {
-
-                // Cão1
-                Button(
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                    contentPadding = PaddingValues(),
-                    modifier = Modifier
-                        .size(100.dp)
-                        .border(
-                            BorderStroke(4.dp, Color.Green),
-                            CircleShape
-                        )
-                        .clip(CircleShape)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.fotocao4),
-                        contentDescription = "botao_redondo"
-                    )
-                }
-            }
-        }
-
-        Box {
-            // Galeria de matchs do cão2
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 70.dp)
-            )
-            {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    contentPadding = PaddingValues(8.dp)
-                ) {
-                    items(match2.size) { index ->
-                        Image(
-                            painter = painterResource(id = match2[index]),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1f)
-                                .padding(4.dp)
-                                .clip(RoundedCornerShape(20.dp))
-                                .background(MaterialTheme.colorScheme.background)
-                                .clickable { /* Handle click if needed */ },
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                }
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Start
-            ) {
-
-                // Cão2
-                Button(
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                    contentPadding = PaddingValues(),
-                    modifier = Modifier
-                        .size(100.dp)
-                        .border(
-                            BorderStroke(4.dp, Color.Green),
-                            CircleShape
-                        )
-                        .clip(CircleShape)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.fotocao2),
-                        contentDescription = "botao_redondo"
-                    )
-                }
             }
         }
 
@@ -178,3 +133,4 @@ fun MatchPorCao(navHostController: NavHostController = rememberNavController()):
         }
     }
 }
+
